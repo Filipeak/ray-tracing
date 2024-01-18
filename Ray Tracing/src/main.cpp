02b7765ebd56447d35b1b9d9f404f7c714ebc767
Filip Gawlik <filippo.gawlik@gmail.com>
2024-01-18 13:44:35 +0100

    Move Todo to README

--- Ray Tracing/src/main.cpp
+++ Ray Tracing/src/main.cpp
@@ -1,30 +1,4 @@
-﻿/**
-* TODO:
-*  [ ] Ray Tracing
-*		- https://www.youtube.com/watch?v=A61S_2swwAc
-*		- https://raytracing.github.io/books/RayTracingInOneWeekend.html
-*		- https://raytracing.github.io/books/RayTracingTheNextWeek.html
-*		- https://raytracing.github.io/books/RayTracingTheRestOfYourLife.html
-*  [ ] Illumination: Diffuse, Direct, Global, Specular
-*  [ ] PBR
-*		- https://www.youtube.com/watch?v=7_iy4Rov2Ck&list=PLlrATfBNZ98edc5GshdBtREv5asFW3yXl&index=10
-*		- https://www.youtube.com/watch?v=AbVfW4X01a0&list=PLlrATfBNZ98edc5GshdBtREv5asFW3yXl&index=15
-*		- https://learnopengl.com/PBR/Theory
-*		- https://learnopengl.com/Guest-Articles/2022/Phys.-Based-Bloom
-*		- https://www.youtube.com/watch?v=XK_p2MxGBQs
-*  [ ] Path Tracing
-*		- https://www.youtube.com/watch?v=NIpC53vesHo&list=PLlrATfBNZ98edc5GshdBtREv5asFW3yXl&index=12
-*  [ ] Camera / Scene abstraction
-*  [ ] Antialiasing
-*		- https://learnopengl.com/Advanced-OpenGL/Anti-Aliasing
-*		- https://raytracing.github.io/books/RayTracingInOneWeekend.html#antialiasing
-*  [ ] BVH Algorithm
-*		- https://www.scratchapixel.com/lessons/3d-basic-rendering/introduction-acceleration-structure/bounding-volume.html
-*  [ ] CUDA / OptiX - Path Tracing, Mesh Ray Tracing
-*  [ ] DirectX 11, 12 / Vulkan
-*/
-
-#include <glad/glad.h>
+﻿#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
