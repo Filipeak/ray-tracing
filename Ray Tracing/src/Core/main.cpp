bcd4cfd5010f111af3eb6f749ad9297758ae30f9
Filip Gawlik <filippo.gawlik@gmail.com>
2025-12-25 18:46:40 +0100

    Fix typo, add bloom mip chain to constructor

--- Ray Tracing/src/Core/main.cpp
+++ Ray Tracing/src/Core/main.cpp
@@ -11,7 +11,7 @@ int main()
 
 	Window window(1920, 1080, "Ray Tracing", config.vsync);
 	Camera camera(window, glm::vec3(0.0f, 0.0f, -10.0f), glm::vec2(0.0f, 0.0f), config.cameraFOV, 0.3f, 1000.0f, config.cameraSpeed, config.cameraSens);
-	RTRenderer renderer(window, camera, config.accumulate, config.hdrExposure, config.screenGamma, config.bloom, config.bloomStrength, config.bloomFilterRadius);
+	RTRenderer renderer(window, camera, config.accumulate, config.hdrExposure, config.screenGamma, config.bloom, config.bloomMipChains, config.bloomStrength, config.bloomFilterRadius);
 	GUI gui(window);
 	gui.AddWindow(new StatsWindow());
 	gui.AddWindow(new ConfigWindow(config, window, renderer, camera));
